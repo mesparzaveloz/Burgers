f3ad3a3a4290747e52f5c86095ba82c58c06650b
jtown <marcelino.esparza@live.com>
2019-06-08 19:13:53 -0500

    update 8

--- config/orm.js
+++ config/orm.js
@@ -5,23 +5,16 @@ var connection = require("../config/connection.js");
 function printQuestionMarks(num) {
   var arr = [];
 
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
-
+  for (var i = 0; i < num; i++) {arr.push("?") }
   return arr.toString();
 }
 
 // Helper function for SQL syntax.
 function objToSql(ob) {
   var arr = [];
-
   for (var key in ob) {
-    if (Object.hasOwnProperty.call(ob, key)) {
-      arr.push(key + "=" + ob[key]);
-    }
+    if (Object.hasOwnProperty.call(ob, key)) {arr.push(key + "=" + ob[key])}
   }
-
   return arr.toString();
 }
 
@@ -49,15 +42,13 @@ var orm = {
     console.log(queryString);
 
     connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
+      if (err) throw err
       cb(result);
     });
   },
+  // An example of objColVals would be {name: panther, sleepy: true}
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
-
     queryString += " SET ";
     queryString += objToSql(objColVals);
     queryString += " WHERE ";
@@ -65,14 +56,11 @@ var orm = {
 
     console.log(queryString);
     connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
+      if (err) throw err
       cb(result);
     });
   }
 };
 
-// Export the orm object for the model.
+// Export the orm object for the model (cat.js).
 module.exports = orm;
